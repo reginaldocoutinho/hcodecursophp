81c3f703c2ada60a022864a75682add316a5c7cc
reginaldocoutinho <reginaldo_re12@hotmail.com>
2024-05-16 20:27:33 -0300

    Operadores Aritméticos

---
+++ operadores/exemplo-03.php
@@ -0,0 +1,31 @@
+<?php
+// Operadores Aritméticos
+$a = 10;
+$b = 2;
+
+
+echo "Aplicações dos operadores</br>(+, -, *, / , %, **):</br></br>";
+
+echo "soma: ". $a + $b;
+
+echo "</br>";
+
+echo "subtração: " . $a - $b;
+
+echo "</br>";
+
+echo "multiplicação: " . $a * $b;
+
+echo "</br>";
+
+echo "divisão: " . $a / $b;
+
+echo "</br>";
+
+echo "resto/módulo: " . $a % $b;
+
+echo "</br>";
+
+echo "elevado: " . $a ** $b;
+
+?>
