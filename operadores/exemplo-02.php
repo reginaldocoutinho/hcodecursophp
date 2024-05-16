73b84e64a141e59fe2deb35d8d5215fe6bff5bda
reginaldocoutinho <reginaldo_re12@hotmail.com>
2024-05-16 19:53:58 -0300

    Calculando valor armazenados com os operadores de +=, -= e *=

---
+++ operadores/exemplo-02.php
@@ -0,0 +1,16 @@
+<?php
+/* mais igual += e o menos igual  -=
+ multiplição igual *=
+*/
+$valorTotal = 0;
+
+$valorTotal += 100;
+
+$valorTotal += 25;
+
+//$valorTotal -= 10;
+
+$valorTotal *= .9;// 90% do valor
+
+echo $valorTotal;
+?>
