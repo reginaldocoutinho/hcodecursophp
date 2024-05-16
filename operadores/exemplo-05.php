7ddc3686fdcdaa8dff7161725bde0d9009b3d99a
reginaldocoutinho <reginaldo_re12@hotmail.com>
2024-05-16 20:59:03 -0300

    Spaceshipping usado para saber entre dois valores se o segundo é maior, igual ou menor que o primeiro de uma forma simples evitando o uso do if

---
+++ operadores/exemplo-05.php
@@ -0,0 +1,22 @@
+<?php
+// Operador spaceshipping
+/*
+    Esse nome é dado pelo formato que resulta do uso do
+    sinal de menor seguido do sinal de igualdade finalizando
+    com o sinal de maior resultando em uma espaço nave
+    <=>
+    Qual o uso?
+    Comparação de dois valores para saber se o segundo valor é
+    maior, igual ou menor que o primeiro valor 
+    sendo que se o primeiro valor for:
+        maior, retorna o 1 
+        igual, retorna 0
+        menor, retorna -1
+*/
+
+$a = 60;
+$b = 80 ;
+
+var_dump($a <=> $b)
+
+?>
