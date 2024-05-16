62732926664da0c7bb81459ac55fd436de270bc4
reginaldocoutinho <reginaldo_re12@hotmail.com>
2024-05-16 19:52:48 -0300

    Operadores de igualdade e concatenação

---
+++ operadores/exemplo-01.php
@@ -0,0 +1,20 @@
+<?php
+/*Operadores de atribuição
+= igualdade
+. concatenação
+.= a variável concatenando com o valor que está a seguir
+*/
+
+$nome = "Hcode";
+echo $nome . " mais alguma coisa";
+
+echo "</br>";
+
+$nome .= " Treinamentos";
+
+echo $nome;
+
+
+
+
+?>
