6d5d37732e93dc5db38a03c669bc8bafccf1312d
reginaldocoutinho <reginaldo_re12@hotmail.com>
2024-07-22 14:07:20 -0300

    Exemplo 01 de POO

---
+++ poo/exemplo-01.php
@@ -0,0 +1,18 @@
+<?php
+
+class Pessoa {
+    
+    public $nome; //Atributo
+
+    public function falar(){ //Método
+
+        return "Meu nome é ". $this->nome;
+    }
+}
+
+$glaucio = new Pessoa();
+
+$glaucio->nome = "Glaucio Daniel";
+echo $glaucio->falar();
+
+?>
